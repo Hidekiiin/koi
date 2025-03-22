b4a77dc5d06b9928c2ff7a27784870d051afd308
Hidekiiin <123300222+Hidekiiin@users.noreply.github.com>
2025-03-22 16:49:45 +0900

    Update socket.js

--- api/socket.js
+++ api/socket.js
@@ -2,21 +2,25 @@ import { Server } from 'socket.io';
 
 export default function handler(req, res) {
   if (!res.socket.server.io) {
+    console.log('🔗 Initializing new Socket.io server...');
     const io = new Server(res.socket.server);
 
     io.on('connection', (socket) => {
-      console.log('User connected');
+      console.log('✅ ユーザー接続しました');
 
       socket.on('chat message', (msg) => {
         io.emit('chat message', msg);
       });
 
       socket.on('disconnect', () => {
-        console.log('User disconnected');
+        console.log('❌ ユーザー切断しました');
       });
     });
 
     res.socket.server.io = io;
+  } else {
+    console.log('⚡️ Socket.io サーバーは既に起動しています');
   }
+
   res.end();
 }
